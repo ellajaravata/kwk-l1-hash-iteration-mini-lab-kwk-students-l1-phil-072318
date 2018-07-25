e26765c559f34d60bfb538fc662ef98774c18e56
Ella Jaravata <ella.jaravata@gmail.com>
2018-07-25 18:45:23 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -3,18 +3,21 @@ def create_olympics_hash
   olympics_place["Sydney", "Athens", "Beijing", "London",]
   olympics_years [2000, 2004, 2008, 2012,]
   summer_olympics{}
+  new_place="Atlanta"
+new_year = 1996
+olympics_place[new_place] = new_year
   
   # Implement this method so that it returns a hash with the data provided on README.md
 end
 create_olympics_hash
 
-def add_a_key_value_pair
-new_place="Atlanta"
-new_year = 1996
-olympics_place[new_place] = new_year
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-end
-add_a_key_value_pair
+# def add_a_key_value_pair
+# new_place="Atlanta"
+# new_year = 1996
+# olympics_place[new_place] = new_year
+#   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+# end
+# add_a_key_value_pair
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
