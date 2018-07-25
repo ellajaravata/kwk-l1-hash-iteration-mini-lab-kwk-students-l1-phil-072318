ad5c079b6598788aa5a7063d8eb355aed6bf9da0
Ella Jaravata <ella.jaravata@gmail.com>
2018-07-25 18:44:34 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -14,7 +14,7 @@ new_year = 1996
 olympics_place[new_place] = new_year
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
 end
-
+add_a_key_value_pair
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
